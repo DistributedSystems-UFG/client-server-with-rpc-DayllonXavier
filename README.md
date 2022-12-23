408b38ff6fee917d709d772436170451e288b8a9
DayllonXavier <dayllonxavier@gmail.com>
2022-12-23 19:06:43 -0300

    Alterações e definição do IP e Porta do Servidor.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Cliente-Servidor com RPC (RPyC)
+Implementação de um serviço (servidor) de manipulação de uma lista, usando RPC, por meio da biblioteca RPyC.
+
+O arquivo "constRPYC.py" contém o IP e a porta do servidor.
+
+O arquivo "server.py" é referente a definição das interfaces e a implementação dos métodos que operam sobre a lista.
+
+O arquivo "client.py" é referente ao código que executa as chamadas (RPC) que operam sobre a lista existente no servidor.
+
+# Vídeo da apresentação
+-
--- client.py
+++ client.py
@@ -3,29 +3,37 @@ from constRPYC import * #-
 
 class Client:
   conn = rpyc.connect(SERVER, PORT)
+
   print (conn.root.exposed_value())
+
   conn.root.exposed_append(6)
   conn.root.exposed_append(4)
   conn.root.exposed_append(3)
   conn.root.exposed_append(2)
   conn.root.exposed_append(1)
   print (conn.root.exposed_value())
+
   conn.root.exposed_insert(2, 5)
   conn.root.exposed_insert(6, 0)
   print (conn.root.exposed_value())
+
   conn.root.exposed_sort()
   print (conn.root.exposed_value())
+
   conn.root.exposed_reverse()
   print (conn.root.exposed_value())
   
   p = conn.root.exposed_search(4)
   print(p)
   print(conn.root.exposed_get(p))
+
   print(conn.root.exposed_length())
 
   conn.root.exposed_remove_idx(p)
   print (conn.root.exposed_value())
+
   conn.root.exposed_remove_idx(2)
   print (conn.root.exposed_value())
+  
   conn.root.exposed_remove_value(1)
   print (conn.root.exposed_value())
